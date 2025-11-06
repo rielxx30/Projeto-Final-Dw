b4eac189c442c0d435e0afc876230bfb78d24ce3
Lizard670 <monarcasemtempo@gmail.com>
2025-11-06 12:48:58 -0300

    Implementação das p´áginas de registrar e logar

---
+++ registrar.php
@@ -0,0 +1,24 @@
+<?php
+    require_once 'funcoes.php';
+    session_start();
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Pega e sanitiza o usuario e senha digitado
+        try {
+            $nome = htmlspecialchars($_POST["nome"]);
+            $cpf = htmlspecialchars($_POST["cpf"]);
+            $email = htmlspecialchars($_POST["email"]);
+            $senha = htmlspecialchars($_POST["senha"]);
+            $senha_novamente = htmlspecialchars($_POST["senha_novamente"]);
+        } catch (Exception $e) {
+            error_log('Erro no request, campos obrigatórios não foram preenchidos');
+            header('pagina registro.php');
+        }
+        $conexao = conectar_bd();
+
+        $salt = random_bytes(32);
+        $criptografada = openssl_pbkdf2($senha, $salt, 32, 600000, 'SHA256');
+        $comando = "INSERT INTO Usuario (Nome, Salt, Senha_hash, Email)  VALUES ('" . $nome . "', '" . $salt . "', '" . $criptografada . "', '" . $email . "')";
+        $resultado_query = mysqli_query($conexao, $comando) or header("Location: login.php");
+    }
+    header('pagina registro.php');
+?>
