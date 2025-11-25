fe976fa36168f835fe24664b9cd18e2f4af1f0bc
Lizandra Amaryllis <202219810018@ifba.edu.br>
2025-11-25 17:20:03 -0300

    Correção no logar.php Correções necessárias por causa das alterações feitas na página de login Não sei se ta funcionando, não da pra testar nesse pc rs

--- backend/logar.php
+++ backend/logar.php
@@ -3,11 +3,11 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Pega e sanitiza o usuario e senha digitado
         try {
-            $usuario = htmlspecialchars($_POST["usuario"]);
+            $email = htmlspecialchars($_POST["email"]);
             $senha = htmlspecialchars($_POST["senha"]);
         } catch (Exception $e) {
             error_log('Erro no request, usuario e/ou senha não estão presentes');
-            header('login.php');
+            header('../paginas/login.php');
         }
 
         $conexao = conectar_bd();
@@ -15,22 +15,22 @@
         session_start();
 
         // Pega o usuário no banco de dados
-        $comando = "SELECT * FROM Usuario  WHERE Email = '" . $usuario . "'";
-        $resultado_query = mysqli_query($conexao, $comando) or header("/paginas/login.php");
+        $comando = "SELECT * FROM Usuario  WHERE Email = '" . $email . "'";
+        $resultado_query = mysqli_query($conexao, $comando) or header("../paginas/login.php");
         if (mysqli_num_rows($resultado_query) < 1) {
             $_SESSION["erro_login"] = "Email ou senha incorretos";
-            header("/paginas/login.php");
+            header("../paginas/login.php");
         }
 
         $usuario_salvo = mysqli_fetch_assoc($resultado_query);
         if (validar_senha($usuario_salvo["idUsuario"], $senha)){
             $_SESSION['usuario'] = $usuario_salvo["nome"];
-            header("/paginas/menu.php");
+            header("../paginas/menu.php");
             exit();
         }
         else{
             $_SESSION["erro_login"] = "Email ou senha incorretos";
-            header("/paginas/login.php");
+            header("../paginas/login.php");
         }
     }
 ?>
